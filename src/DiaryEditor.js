91f3954559e8e435320dca11a872fc5354ec492d
YUJIN JI <yuzzzin@gmail.com>
2023-07-19 12:18:46 +0900

    Update DiaryEditor.js

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,6 +1,10 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 const DiaryEditor = () => {
+  // DOM 요소를 선택하는 useRef (react에서 DOM 조작하기)
+  const authorInput = useRef();
+  const contentInput = useRef();
+
   const [state, setState] = useState({
     author: '',
     content: '',
@@ -8,9 +12,6 @@ const DiaryEditor = () => {
   });
 
   const handleChangeState = (e) => {
-    console.log(e.target.name);
-    console.log(e.target.value);
-
     setState({
       ...state,
       [e.target.name]: e.target.value,
@@ -18,7 +19,20 @@ const DiaryEditor = () => {
   };
 
   const handleSubmit = () => {
-    console.log(state);
+    if (state.author.length < 1) {
+      // alert('작성자명은 최소 1글자 이상 입력해주세요');
+      // focus
+      // alert 대신 focus 효과로 대신해보기
+      authorInput.current.focus();
+      return;
+    }
+
+    if (state.content.length < 5) {
+      // focus
+      contentInput.current.focus();
+      return;
+    }
+
     alert('저장 성공 :)');
   };
 
@@ -27,6 +41,8 @@ const DiaryEditor = () => {
       <h2>오늘의 일기</h2>
       <div>
         <input
+          ref={authorInput}
+          placeholder="작성자명을 입력해주세요"
           name="author"
           value={state.author}
           onChange={handleChangeState}
@@ -34,6 +50,7 @@ const DiaryEditor = () => {
       </div>
       <div>
         <textarea
+          ref={contentInput}
           placeholder="오늘의 일들을 적어보세요 :)"
           name="content"
           value={state.content}
